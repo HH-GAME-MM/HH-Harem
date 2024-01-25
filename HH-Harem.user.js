29ce2e5501282fd7306a9a6b13bf075a9b207d03
-MM- <107755486+HH-GAME-MM@users.noreply.github.com>
2024-01-25 18:06:24 +0100

    v0.11

--- HH-Harem.user.js
+++ HH-Harem.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         HH Harem
-// @version      0.9
+// @version      0.11
 // @description  Compact Harem filter, "open by default" setting, open the girl upgrade page in a new tab by double-clicking
 // @author       -MM-
 // @match        https://*.hentaiheroes.com/harem.html
 // @match        https://*.hentaiheroes.com/harem/*
 // @match        https://nutaku.haremheroes.com/harem.html
 // @match        https://nutaku.haremheroes.com/harem/*
-// @match        https://*.comixharem.com/harem.html
-// @match        https://*.comixharem.com/harem/*
-// @match        https://*.pornstarharem.com/harem.html
-// @match        https://*.pornstarharem.com/harem/*
+// @match        https://www.comixharem.com/harem.html
+// @match        https://www.comixharem.com/harem/*
+// @match        https://www.pornstarharem.com/harem.html
+// @match        https://www.pornstarharem.com/harem/*
 // @run-at       document-end
 // @namespace    https://github.com/HH-GAME-MM/HH-Harem
 // @updateURL    https://github.com/HH-GAME-MM/HH-Harem/raw/main/HH-Harem.user.js
@@ -24,38 +24,40 @@
 
     console.log('HH Harem Script v' + GM_info.script.version);
 
-    //double click a girl to open girl upgrade page in a new tab
-    let girls_list = document.querySelector('#harem_left div.girls_list');
-    girls_list.addEventListener('dblclick', (e) => {
-        let target = e.target;
-        if(target != girls_list)
+    setTimeout(() => {
+        //double click a girl to open girl upgrade page in a new tab
+        let girls_list = document.querySelector('#harem_left div.girls_list');
+        girls_list.addEventListener('dblclick', (e) => {
+            let target = e.target;
+            if(target != girls_list)
+            {
+                while(target.getAttribute('id_girl') == null) target = target.parentNode;
+                window.open('https://'+window.location.hostname+'/girl/'+target.getAttribute('id_girl')+'?resource=experience', '_blank');
+            }
+        });
+
+        //filter css
+        let css = document.createElement('style');
+        document.head.appendChild(css);
+        css.sheet.insertRule('#filtering_girls div.form-wrapper div.form-control div.checkbox-group button.check-btn {height:34px}');
+        css.sheet.insertRule('#filtering_girls div.form-wrapper div.form-control div.checkbox-group button.check-btn.shards-state div {margin-top:-6px}');
+
+        //filter status
+        let filterStatus = localStorage.getItem('HHHarem_FilterStatus');
+        if(filterStatus == null) filterStatus = 'Closed';
+        let formControlFilterStatus = document.createElement('div');
+        formControlFilterStatus.setAttribute('class', 'form-control double-wide');
+        formControlFilterStatus.setAttribute('style', 'width:100%;height:50px;margin-top:-17px');
+        formControlFilterStatus.innerHTML = '<div class="select-group"><label class="head-group" for="lists-select">Filter Status</label><select icon="down-arrow" onchange="localStorage.setItem(\'HHHarem_FilterStatus\', this.value)"><option value="Open"' + (filterStatus == 'Open' ? ' selected="selected"' : '') + '>Open by default</option><option value="Closed"' + (filterStatus == 'Closed' ? ' selected="selected"' : '') + '>Closed by default</option></select></div>';
+        document.querySelector('#filtering_girls div.form-wrapper').appendChild(formControlFilterStatus);
+
+        //open the filter if necessary
+        if(filterStatus == 'Open')
         {
-            while(target.getAttribute('id_girl') == null) target = target.parentNode;
-            window.open('https://'+window.location.hostname+'/girl/'+target.getAttribute('id_girl')+'?resource=experience', '_blank');
+            document.getElementById('filtering_girls').setAttribute('class', 'original_position');
+            document.getElementById('harem_left').setAttribute('class', 'original_position');
+            document.getElementById('harem_right').setAttribute('class', 'original_position');
+            document.querySelector('#filter_girls span').setAttribute('class', 'search_close_icn');
         }
-    });
-
-    //filter css
-    let css = document.createElement('style');
-    document.head.appendChild(css);
-    css.sheet.insertRule('#filtering_girls div.form-wrapper div.form-control div.checkbox-group button.check-btn {height:34px}');
-    css.sheet.insertRule('#filtering_girls div.form-wrapper div.form-control div.checkbox-group button.check-btn.shards-state div {margin-top:-6px}');
-
-    //filter status
-    let filterStatus = localStorage.getItem('HHHarem_FilterStatus');
-    if(filterStatus == null) filterStatus = 'Closed';
-    let formControlFilterStatus = document.createElement('div');
-    formControlFilterStatus.setAttribute('class', 'form-control double-wide');
-    formControlFilterStatus.setAttribute('style', 'width:100%;height:50px;margin-top:-17px');
-    formControlFilterStatus.innerHTML = '<div class="select-group"><label class="head-group" for="lists-select">Filter Status</label><select icon="down-arrow" onchange="localStorage.setItem(\'HHHarem_FilterStatus\', this.value)"><option value="Open"' + (filterStatus == 'Open' ? ' selected="selected"' : '') + '>Open by default</option><option value="Closed"' + (filterStatus == 'Closed' ? ' selected="selected"' : '') + '>Closed by default</option></select></div>';
-    document.querySelector('#filtering_girls div.form-wrapper').appendChild(formControlFilterStatus);
-
-    //open the filter if necessary
-    if(filterStatus == 'Open')
-    {
-        document.getElementById('filtering_girls').setAttribute('class', 'original_position');
-        document.getElementById('harem_left').setAttribute('class', 'original_position');
-        document.getElementById('harem_right').setAttribute('class', 'original_position');
-        document.querySelector('#filter_girls span').setAttribute('class', 'search_close_icn');
-    }
+    }, 1);
 })();
