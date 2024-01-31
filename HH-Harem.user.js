26bc934211d85a7263bce69aee2fe0408eec7fd2
-MM- <107755486+HH-GAME-MM@users.noreply.github.com>
2024-01-31 12:28:22 +0100

    v0.12

--- HH-Harem.user.js
+++ HH-Harem.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HH Harem
-// @version      0.11
+// @version      0.12
 // @description  Compact Harem filter, "open by default" setting, open the girl upgrade page in a new tab by double-clicking
 // @author       -MM-
 // @match        https://*.hentaiheroes.com/harem.html
@@ -39,15 +39,16 @@
         //filter css
         let css = document.createElement('style');
         document.head.appendChild(css);
-        css.sheet.insertRule('#filtering_girls div.form-wrapper div.form-control div.checkbox-group button.check-btn {height:34px}');
-        css.sheet.insertRule('#filtering_girls div.form-wrapper div.form-control div.checkbox-group button.check-btn.shards-state div {margin-top:-6px}');
+        css.sheet.insertRule('#filtering_girls .form-wrapper .form-control .checkbox-group .check-btn.shards-state div {margin-top:-2px !important}');
+        css.sheet.insertRule('#filtering_girls .form-wrapper .form-control .checkbox-group .check-btn.shards-state, #filtering_girls .form-wrapper .form-control .checkbox-group .check-btn.carac-state {height:1.7rem !important;}');
+        css.sheet.insertRule('#filtering_girls .form-wrapper .checkbox-group:nth-of-type(2) {margin-bottom:0px !important}');
 
         //filter status
         let filterStatus = localStorage.getItem('HHHarem_FilterStatus');
         if(filterStatus == null) filterStatus = 'Closed';
         let formControlFilterStatus = document.createElement('div');
-        formControlFilterStatus.setAttribute('class', 'form-control double-wide');
-        formControlFilterStatus.setAttribute('style', 'width:100%;height:50px;margin-top:-17px');
+        formControlFilterStatus.setAttribute('class', 'form-control');
+        formControlFilterStatus.setAttribute('style', 'grid-column:1/3;width:21rem;max-width:21rem;margin-bottom:5px');
         formControlFilterStatus.innerHTML = '<div class="select-group"><label class="head-group" for="lists-select">Filter Status</label><select icon="down-arrow" onchange="localStorage.setItem(\'HHHarem_FilterStatus\', this.value)"><option value="Open"' + (filterStatus == 'Open' ? ' selected="selected"' : '') + '>Open by default</option><option value="Closed"' + (filterStatus == 'Closed' ? ' selected="selected"' : '') + '>Closed by default</option></select></div>';
         document.querySelector('#filtering_girls div.form-wrapper').appendChild(formControlFilterStatus);
 
